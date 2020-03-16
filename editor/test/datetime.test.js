0a0f9b339dbd7d51f9db596edb00509cdf7c10b9
Randy Merrill <randy@blinkk.com>
2020-03-16 16:53:20 -0600

    Adding datetime field tests.

---
+++ editor/test/datetime.test.js
@@ -0,0 +1,169 @@
+const defaults = require('./defaults')
+const { percySnapshot } = require('@percy/puppeteer')
+const path = require('path')
+const qs = require('querystring')
+
+const editorConfig = {
+  'fields': [
+    {
+      'type': 'datetime',
+      'key': 'published',
+      'label': 'Published',
+    }
+  ]
+}
+const defaultEn = ''
+const defaultEs = ''
+let newValueEn = '2021-04-02T14:05'
+let newValueEs = '2020-07-25T05:18'
+
+// Format the typing for the order that the field shows in en_US.
+let newDateTypeEn = '04-02-2021'
+let newDateTypeEs = '07-25-2020'
+let newTimeTypeEn = '02-05p'
+let newTimeTypeEs = '05-18a'
+
+describe('datetime field', () => {
+  beforeEach(async () => {
+    // Need a new page to prevent requests already being handled.
+    page = await browser.newPage()
+    await page.goto('http://localhost:3000/editor.html')
+    await page.setRequestInterception(true)
+
+    page.on('request', request => {
+      if (request.url().includes('/_grow/api/editor/content')) {
+        console.log('Intercepted content', request.url(), request.method())
+        if (request.method() == 'POST') {
+          // Respond to posts with the same front matter.
+          const postData = qs.parse(request.postData())
+          const frontMatter = JSON.parse(postData.front_matter)
+          request.respond({
+            contentType: 'application/json',
+            body: JSON.stringify(Object.assign({}, defaults.documentResponse, {
+              'front_matter': frontMatter,
+              'editor': editorConfig,
+            }))
+          })
+        } else {
+          request.respond({
+            contentType: 'application/json',
+            body: JSON.stringify(Object.assign({}, defaults.documentResponse, {
+              'front_matter': {
+                'published': defaultEn,
+                'published@es': defaultEs,
+              },
+              'editor': editorConfig,
+            }))
+          })
+        }
+      } else {
+        // console.log('Piped request', request.url(), request.method())
+        request.continue()
+      }
+    })
+
+    await page.evaluate(_ => {
+      window.editorInst = new Editor(document.querySelector('.container'), {
+        'testing': true,
+      })
+    })
+    await page.waitForSelector('.selective__fields')
+  })
+
+  it('should accept input', async () => {
+    // Editor starts out clean.
+    let isClean = await page.evaluate(_ => {
+      return window.editorInst.isClean
+    })
+    expect(isClean).toBe(true)
+
+    // Change the title.
+    await page.click('.selective__field__datetime input')
+    await page.keyboard.type(newDateTypeEn.replace('-', ''))
+    await page.keyboard.press('Tab')
+    await page.keyboard.type(newTimeTypeEn.replace('-', ''))
+
+    // Editor should now be dirty.
+    isClean = await page.evaluate(_ => {
+      return window.editorInst.isClean
+    })
+    expect(isClean).toBe(false)
+
+    // Save the changes.
+    const saveButton = await page.$('.editor__save')
+    await saveButton.click()
+    await page.waitForSelector('.editor__save--saving')
+    await page.waitForSelector('.editor__save:not(.editor__save--saving)')
+
+    // Verify the new value was saved.
+    const value = await page.evaluate(_ => {
+      return window.editorInst.selective.value
+    })
+    expect(value).toMatchObject({
+      'published': newValueEn,
+      'published@es': defaultEs,
+    })
+
+    // After saving the editor should be clean.
+    isClean = await page.evaluate(_ => {
+      return window.editorInst.isClean
+    })
+    expect(isClean).toBe(true)
+
+    await percySnapshot(page, 'Datetime field after save', defaults.snapshotOptions)
+  })
+
+  it('should accept input on localization', async () => {
+    // Editor starts out clean.
+    let isClean = await page.evaluate(_ => {
+      return window.editorInst.isClean
+    })
+    expect(isClean).toBe(true)
+
+    // Enable localization.
+    const localizationIcon = await page.$('i[title="Localize content"]')
+    await localizationIcon.click()
+    await page.waitForSelector('.selective__field__datetime input[data-locale=en]')
+
+    // Change the en title.
+    await page.click('.selective__field__datetime input[data-locale=en]')
+    await page.keyboard.type(newDateTypeEn.replace('-', ''))
+    await page.keyboard.press('Tab')
+    await page.keyboard.type(newTimeTypeEn.replace('-', ''))
+
+    // Change the es title.
+    await page.click('.selective__field__datetime input[data-locale=es]')
+    await page.keyboard.type(newDateTypeEs.replace('-', ''))
+    await page.keyboard.press('Tab')
+    await page.keyboard.type(newTimeTypeEs.replace('-', ''))
+
+    // Editor should now be dirty.
+    isClean = await page.evaluate(_ => {
+      return window.editorInst.isClean
+    })
+    expect(isClean).toBe(false)
+
+    // Save the changes.
+    const saveButton = await page.$('.editor__save')
+    await saveButton.click()
+    await page.waitForSelector('.editor__save--saving')
+    await page.waitForSelector('.editor__save:not(.editor__save--saving)')
+
+    // Verify the new value was saved.
+    const value = await page.evaluate(_ => {
+      return window.editorInst.selective.value
+    })
+    expect(value).toMatchObject({
+      'published': newValueEn,
+      'published@es': newValueEs,
+    })
+
+    // After saving the editor should be clean.
+    isClean = await page.evaluate(_ => {
+      return window.editorInst.isClean
+    })
+    expect(isClean).toBe(true)
+
+    await percySnapshot(page, 'Datetime field after localization save', defaults.snapshotOptions)
+  })
+})
