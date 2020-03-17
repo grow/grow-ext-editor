bd5c1758cbca6f9b8ba0d5b5a137a8210fb49103
Randy Merrill <randy@blinkk.com>
2020-03-17 11:13:08 -0600

    Adding tests for group with a key.

---
+++ editor/test/structure/group.key.test.js
@@ -0,0 +1,183 @@
+const defaults = require('../defaults')
+const { percySnapshot } = require('@percy/puppeteer')
+const path = require('path')
+const qs = require('querystring')
+
+const editorConfig = {
+  'fields': [
+    {
+      'type': 'group',
+      'key': 'group',
+      'label': 'Group',
+      'fields': [
+        {
+          'type': 'text',
+          'key': 'title',
+          'label': 'Title',
+        }
+      ],
+    }
+  ]
+}
+const defaultEn = 'Trumpet'
+const defaultEs = 'Trompeta'
+let newValueEn = 'Trombone'
+let newValueEs = 'Trombón'
+
+describe('group field with key', () => {
+  beforeEach(async () => {
+    // Need a new page to prevent requests already being handled.
+    page = await browser.newPage()
+    await page.goto('http://localhost:3000/editor.html')
+    await page.setRequestInterception(true)
+
+    page.on('request', request => {
+      if (request.url().includes('/_grow/api/editor/content')) {
+        console.log('Intercepted content', request.url(), request.method())
+        if (request.method() == 'POST') {
+          // Respond to posts with the same front matter.
+          const postData = qs.parse(request.postData())
+          const frontMatter = JSON.parse(postData.front_matter)
+          request.respond({
+            contentType: 'application/json',
+            body: JSON.stringify(Object.assign({}, defaults.documentResponse, {
+              'front_matter': frontMatter,
+              'editor': editorConfig,
+            }))
+          })
+        } else {
+          request.respond({
+            contentType: 'application/json',
+            body: JSON.stringify(Object.assign({}, defaults.documentResponse, {
+              'front_matter': {
+                'group': {
+                  'title': defaultEn,
+                  'title@es': defaultEs,
+                },
+              },
+              'editor': editorConfig,
+            }))
+          })
+        }
+      } else {
+        // console.log('Piped request', request.url(), request.method())
+        request.continue()
+      }
+    })
+
+    await page.evaluate(_ => {
+      window.editorInst = new Editor(document.querySelector('.container'), {
+        'testing': true,
+      })
+    })
+    await page.waitForSelector('.selective__fields')
+  })
+
+  it('should accept input', async () => {
+    // Editor starts out clean.
+    let isClean = await page.evaluate(_ => {
+      return window.editorInst.isClean
+    })
+    expect(isClean).toBe(true)
+
+    await percySnapshot(page, 'Group field before expand', defaults.snapshotOptions)
+
+    // Expand the group.
+    const groupLabel = await page.$('.selective__field__group label')
+    await groupLabel.click()
+
+    // Change the title.
+    await page.click('.selective__field__text input', {clickCount: 3})
+    await page.keyboard.press('Backspace')
+    await page.keyboard.type(newValueEn)
+
+    // Editor should now be dirty.
+    isClean = await page.evaluate(_ => {
+      return window.editorInst.isClean
+    })
+    expect(isClean).toBe(false)
+
+    // Save the changes.
+    const saveButton = await page.$('.editor__save')
+    await saveButton.click()
+    await page.waitForSelector('.editor__save--saving')
+    await page.waitForSelector('.editor__save:not(.editor__save--saving)')
+
+    // Verify the new value was saved.
+    const value = await page.evaluate(_ => {
+      return window.editorInst.selective.value
+    })
+    expect(value).toMatchObject({
+      'group': {
+        'title': newValueEn,
+        'title@es': defaultEs,
+      },
+    })
+
+    // After saving the editor should be clean.
+    isClean = await page.evaluate(_ => {
+      return window.editorInst.isClean
+    })
+    expect(isClean).toBe(true)
+
+    await percySnapshot(page, 'Group field after save', defaults.snapshotOptions)
+  })
+
+  it('should accept input on localization', async () => {
+    // Editor starts out clean.
+    let isClean = await page.evaluate(_ => {
+      return window.editorInst.isClean
+    })
+    expect(isClean).toBe(true)
+
+    // Expand the group.
+    const groupLabel = await page.$('.selective__field__group label')
+    await groupLabel.click()
+
+    // Enable localization.
+    const localizationIcon = await page.$('i[title="Localize content"]')
+    await localizationIcon.click()
+    await page.waitForSelector('.selective__field__text input[data-locale=en]')
+
+    // Change the en title.
+    await page.click('.selective__field__text input[data-locale=en]', {clickCount: 3})
+    await page.keyboard.press('Backspace')
+    await page.keyboard.type(newValueEn)
+
+    // Change the es title.
+    await page.click('.selective__field__text input[data-locale=es]', {clickCount: 3})
+    await page.keyboard.press('Backspace')
+    await page.keyboard.type(newValueEs)
+
+    // Editor should now be dirty.
+    isClean = await page.evaluate(_ => {
+      return window.editorInst.isClean
+    })
+    expect(isClean).toBe(false)
+
+    // Save the changes.
+    const saveButton = await page.$('.editor__save')
+    await saveButton.click()
+    await page.waitForSelector('.editor__save--saving')
+    await page.waitForSelector('.editor__save:not(.editor__save--saving)')
+
+    // Verify the new value was saved.
+    const value = await page.evaluate(_ => {
+      return window.editorInst.selective.value
+    })
+    expect(value).toMatchObject({
+      'group': {
+        'title': newValueEn,
+        'title@es': newValueEs,
+      },
+    })
+
+    // After saving the editor should be clean.
+    isClean = await page.evaluate(_ => {
+      return window.editorInst.isClean
+    })
+    expect(isClean).toBe(true)
+
+    await percySnapshot(page, 'Group field after localization save', defaults.snapshotOptions)
+  })
+})
