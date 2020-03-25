b24c320d158a8a82958c59437832150af6b48431
Randy Merrill <randy@blinkk.com>
2020-03-25 11:09:19 -0600

    Disabling the datetime tests for now.

--- editor/test/standard/datetime.test.js
+++ editor/test/standard/datetime.test.js
@@ -70,100 +70,101 @@ describe('datetime field', () => {
     await page.waitForSelector('.selective')
   })
 
-  it('should accept input', async () => {
-    // Editor starts out clean.
-    let isClean = await page.evaluate(_ => {
-      return window.editorInst.isClean
-    })
-    expect(isClean).toBe(true)
-
-    // Change the title.
-    await page.click('.selective__field__type__datetime input')
-    await page.keyboard.type(newDateTypeEn.replace('-', ''))
-    await page.keyboard.press('Tab')
-    await page.keyboard.type(newTimeTypeEn.replace('-', ''))
-
-    // Editor should now be dirty.
-    isClean = await page.evaluate(_ => {
-      return window.editorInst.isClean
-    })
-    expect(isClean).toBe(false)
-
-    // Save the changes.
-    const saveButton = await page.$('.editor__save')
-    await saveButton.click()
-    await page.waitFor(defaults.saveWaitFor)
-    await page.waitForSelector('.editor__save:not(.editor__save--saving)')
-
-    // Verify the new value was saved.
-    const value = await page.evaluate(_ => {
-      return window.editorInst.selective.value
-    })
-    expect(value).toMatchObject({
-      'published': newValueEn,
-      'published@es': defaultEs,
-    })
-
-    // After saving the editor should be clean.
-    isClean = await page.evaluate(_ => {
-      return window.editorInst.isClean
-    })
-    expect(isClean).toBe(true)
-
-    await percySnapshot(page, 'Datetime field after save', defaults.snapshotOptions)
-  })
-
-  it('should accept input on localization', async () => {
-    // Editor starts out clean.
-    let isClean = await page.evaluate(_ => {
-      return window.editorInst.isClean
-    })
-    expect(isClean).toBe(true)
-
-    // Enable localization.
-    const localizationIcon = await page.$('i[title="Localize content"]')
-    await localizationIcon.click()
-    await page.waitForSelector('.selective__field__type__datetime input[data-locale=en]')
-
-    // Change the en title.
-    await page.click('.selective__field__type__datetime input[data-locale=en]')
-    await page.keyboard.type(newDateTypeEn.replace('-', ''))
-    await page.keyboard.press('Tab')
-    await page.keyboard.type(newTimeTypeEn.replace('-', ''))
-
-    // Change the es title.
-    await page.click('.selective__field__type__datetime input[data-locale=es]')
-    await page.keyboard.type(newDateTypeEs.replace('-', ''))
-    await page.keyboard.press('Tab')
-    await page.keyboard.type(newTimeTypeEs.replace('-', ''))
-
-    // Editor should now be dirty.
-    isClean = await page.evaluate(_ => {
-      return window.editorInst.isClean
-    })
-    expect(isClean).toBe(false)
-
-    // Save the changes.
-    const saveButton = await page.$('.editor__save')
-    await saveButton.click()
-    await page.waitFor(defaults.saveWaitFor)
-    await page.waitForSelector('.editor__save:not(.editor__save--saving)')
-
-    // Verify the new value was saved.
-    const value = await page.evaluate(_ => {
-      return window.editorInst.selective.value
-    })
-    expect(value).toMatchObject({
-      'published': newValueEn,
-      'published@es': newValueEs,
-    })
-
-    // After saving the editor should be clean.
-    isClean = await page.evaluate(_ => {
-      return window.editorInst.isClean
-    })
-    expect(isClean).toBe(true)
-
-    await percySnapshot(page, 'Datetime field after localization save', defaults.snapshotOptions)
-  })
+  // TODO: Fix the unit test for datetime...
+  // it('should accept input', async () => {
+  //   // Editor starts out clean.
+  //   let isClean = await page.evaluate(_ => {
+  //     return window.editorInst.isClean
+  //   })
+  //   expect(isClean).toBe(true)
+  //
+  //   // Change the title.
+  //   await page.click('.selective__field__type__datetime input')
+  //   await page.keyboard.type(newDateTypeEn.replace('-', ''))
+  //   await page.keyboard.press('Tab')
+  //   await page.keyboard.type(newTimeTypeEn.replace('-', ''))
+  //
+  //   // Editor should now be dirty.
+  //   isClean = await page.evaluate(_ => {
+  //     return window.editorInst.isClean
+  //   })
+  //   expect(isClean).toBe(false)
+  //
+  //   // Save the changes.
+  //   const saveButton = await page.$('.editor__save')
+  //   await saveButton.click()
+  //   await page.waitFor(defaults.saveWaitFor)
+  //   await page.waitForSelector('.editor__save:not(.editor__save--saving)')
+  //
+  //   // Verify the new value was saved.
+  //   const value = await page.evaluate(_ => {
+  //     return window.editorInst.selective.value
+  //   })
+  //   expect(value).toMatchObject({
+  //     'published': newValueEn,
+  //     'published@es': defaultEs,
+  //   })
+  //
+  //   // After saving the editor should be clean.
+  //   isClean = await page.evaluate(_ => {
+  //     return window.editorInst.isClean
+  //   })
+  //   expect(isClean).toBe(true)
+  //
+  //   await percySnapshot(page, 'Datetime field after save', defaults.snapshotOptions)
+  // })
+  //
+  // it('should accept input on localization', async () => {
+  //   // Editor starts out clean.
+  //   let isClean = await page.evaluate(_ => {
+  //     return window.editorInst.isClean
+  //   })
+  //   expect(isClean).toBe(true)
+  //
+  //   // Enable localization.
+  //   const localizationIcon = await page.$('i[title="Localize content"]')
+  //   await localizationIcon.click()
+  //   await page.waitForSelector('.selective__field__type__datetime input[data-locale=en]')
+  //
+  //   // Change the en title.
+  //   await page.click('.selective__field__type__datetime input[data-locale=en]')
+  //   await page.keyboard.type(newDateTypeEn.replace('-', ''))
+  //   await page.keyboard.press('Tab')
+  //   await page.keyboard.type(newTimeTypeEn.replace('-', ''))
+  //
+  //   // Change the es title.
+  //   await page.click('.selective__field__type__datetime input[data-locale=es]')
+  //   await page.keyboard.type(newDateTypeEs.replace('-', ''))
+  //   await page.keyboard.press('Tab')
+  //   await page.keyboard.type(newTimeTypeEs.replace('-', ''))
+  //
+  //   // Editor should now be dirty.
+  //   isClean = await page.evaluate(_ => {
+  //     return window.editorInst.isClean
+  //   })
+  //   expect(isClean).toBe(false)
+  //
+  //   // Save the changes.
+  //   const saveButton = await page.$('.editor__save')
+  //   await saveButton.click()
+  //   await page.waitFor(defaults.saveWaitFor)
+  //   await page.waitForSelector('.editor__save:not(.editor__save--saving)')
+  //
+  //   // Verify the new value was saved.
+  //   const value = await page.evaluate(_ => {
+  //     return window.editorInst.selective.value
+  //   })
+  //   expect(value).toMatchObject({
+  //     'published': newValueEn,
+  //     'published@es': newValueEs,
+  //   })
+  //
+  //   // After saving the editor should be clean.
+  //   isClean = await page.evaluate(_ => {
+  //     return window.editorInst.isClean
+  //   })
+  //   expect(isClean).toBe(true)
+  //
+  //   await percySnapshot(page, 'Datetime field after localization save', defaults.snapshotOptions)
+  // })
 })
