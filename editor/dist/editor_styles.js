62c90a24feb7d846d0a8c2be2c1049f2e6083c37
Randy Merrill <randy@blinkk.com>
2020-03-30 15:26:00 -0600

    Fixing the variable names.

--- editor/dist/editor_styles.js
+++ editor/dist/editor_styles.js
@@ -91,9 +91,9 @@
   !*** ./source/composite/editor_styles.sass ***!
   \*********************************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Undefined variable: \"$ge-speed-faster\".\n        on line 38 of source/styles/menu/trees.sass\n        from line 4 of source/styles/menu.sass\n        from line 2 of /Users/randy/code/blinkk/ext/editor/editor/source/composite/editor_styles.sass\n>>   transition: all $ge-speed-faster ease-in-out;\n\n   ------------------^\n\n    at /Users/randy/code/blinkk/ext/editor/editor/node_modules/webpack/lib/NormalModule.js:316:20\n    at /Users/randy/code/blinkk/ext/editor/editor/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/randy/code/blinkk/ext/editor/editor/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Users/randy/code/blinkk/ext/editor/editor/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.callback (/Users/randy/code/blinkk/ext/editor/editor/node_modules/sass-loader/dist/index.js:73:7)\n    at Object.done [as callback] (/Users/randy/code/blinkk/ext/editor/editor/node_modules/neo-async/async.js:8067:18)\n    at options.error (/Users/randy/code/blinkk/ext/editor/editor/node_modules/node-sass/lib/index.js:294:32)");
+// extracted by mini-css-extract-plugin
 
 /***/ })
 
